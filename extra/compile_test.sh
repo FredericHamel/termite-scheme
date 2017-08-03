3d0bd37847d861b44d6d22f0baf5b10c78d971cb
FredericHamel <frederic.hamel123@gmail.com>
2017-08-03 11:31:45 +0000

    shell script to compile tests.

---
+++ extra/compile_test.sh
@@ -0,0 +1,108 @@
+#!/bin/sh
+
+output=(_termite.log)
+exe=()
+obj=()
+
+c_exe () {
+  if (( $# == 1 )); then
+    output[${#output[@]}]=${1%.*}
+    exe[${#exe[@]}]=$1
+  fi
+}
+
+c_obj () {
+  if (( $# == 1 )); then
+    output[${#output[@]}]=${1%.*}.o1
+    obj[${#obj[@]}]=$1
+  fi
+}
+
+c_clean() {
+  if (( ${#output[@]} > 0 )); then
+    for i in "${output[@]}"; do
+      if [[ -r $i ]]; then
+        echo "Cleaning $i"
+        rm  $i
+      fi
+    done
+  fi
+}
+
+c_compile_file() {
+  if (( $# >= 3 )); then
+    if [[ -r $3 ]]; then
+      local in_modify_time=`stat -c %Y $2`
+      local out_modify_time=`stat -c %Y $3`
+      if (( $in_modify_time > $out_modify_time )); then
+        echo $1
+        rm $3
+        tsic $4 $2
+      fi
+    else
+      echo $1
+      tsic $4 $2
+    fi
+  fi
+}
+
+c_compile() {
+  local out
+  local i
+  for i in ${obj[@]}; do
+    out=${i%.*}.o1
+    c_compile_file "Compiling object $i" $i $out
+    if (( $? != 0 )); then
+      exit 1
+    fi
+  done
+  for i in "${exe[@]}"; do
+    out=${i%.*}
+    c_compile_file "Compiling executable $i" $i $out -exe
+    if (( $? != 0 )); then
+      exit 1
+    fi
+  done
+}
+
+c_add_all() {
+  if [ -r $PWD/config.cfg ]; then
+    source "$PWD/config.cfg"
+  else
+    echo "config.cfg not found..."
+  fi
+}
+
+c_clean
+
+if (( $# == 0 )); then
+  c_add_all
+  c_compile
+else
+  case $1 in
+    -c)
+      c_add_all
+      c_clean
+      ;;
+    -e)
+      if (( $# == 2 )); then
+        c_exe $2
+        c_compile
+      else
+        echo "Invalid number of arguments $#"
+      fi
+      ;;
+    -o)
+      if (( $# == 2 )); then
+        c_obj $2
+        c_clean
+        c_compile
+      else
+        echo "Invalid number of arguments $#"
+      fi
+      ;;
+    *)
+      echo "Invalid argument $1"
+      ;;
+  esac
+fi
